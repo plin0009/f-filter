378e763f5bb9891abc976024a63f15de0c49e1c1
Peter Lin <plin0009@gmail.com>
2020-05-30 13:18:47 -0400

    Set up react-router and cleaned up the template

---
+++ web/src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main className="App">
+      <h1>About</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main className="App">
+      <h1>Home</h1>
+    </main>
+  );
+};
+
+export default HomePage;
