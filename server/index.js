26bdaf5d2a57d291bffaedff2cc9f925a27bc8da
Peter Lin <plin0009@gmail.com>
2020-05-31 11:51:15 -0400

    Server starts on proper port for Heroku

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import { Filter } from "./models";
 import { generateCode } from "./CodeGenerator";
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
